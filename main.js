fa68d741fb25ab95c700b98a7aded8b2c11899f5
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-02-07 17:06:15 +0100

    cleanup

--- main.js
+++ main.js
@@ -6,11 +6,7 @@ import "swiper/css";
 const swiper = new Swiper(".swiper", {
   slidesPerView: 4,
   initialSlide:0,
-  // spaceBetween: 10,
-  // speed: 250,
-  // // clickable: true,
-  // // watchOverflow: true,
-  // centeredSlides:true,
+  speed: 850,
   loop: true,
 
 
