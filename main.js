62ba8603b3c25ea07dbe3c18d00c8ec24bef8efe
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-02-08 14:50:15 +0100

    slider done

--- main.js
+++ main.js
@@ -1,21 +1,63 @@
+const token = "Bearer 4e8b5ccf15e108ada37b3117bf67c112";
+import axios from "axios";
 import Swiper from "swiper";
+import "swiper/css";
 import { Navigation } from "swiper/modules";
 
-import "swiper/css";
+const videoURL = `https://api.vimeo.com/videos?query=824804225`;
+const countVideoFrames = 8;
 
 const swiper = new Swiper(".swiper", {
   slidesPerView: 4,
-  initialSlide:0,
+  initialSlide: 0,
   speed: 850,
   loop: true,
 
-
-  modules: [Navigation],
+  modules: [Navigation, ],
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-  lazy: {
-    loadPrevNext: true,
-  },
 });
+
+const options = {
+  headers: {
+    Authorization: token,
+    "Content-Type": "application/json",
+  },
+};
+
+async function getVideo() {
+  try {
+    let response = await axios.get(videoURL, options);
+    if (response.status === 200) {
+      response = response.data.data[0];
+      // console.log(response)
+      const videoContainer = document.querySelector(".swiper-wrapper");
+      let htmlToInsert = "";
+      const slideStartTag = "<div class='swiper-slide'>";
+      const slideEndTag = "</div>";
+      for (let i = 1; i <= countVideoFrames; i++) {
+        htmlToInsert += slideStartTag + response.embed.html + slideEndTag;
+      }
+      videoContainer.insertAdjacentHTML("afterbegin", htmlToInsert);
+
+      const iframes = document.querySelectorAll("iframe");
+      iframes.forEach((el) => {
+        el.setAttribute("height", "300px"),
+        el.setAttribute("controls", 0),
+        el.setAttribute("allow", "fullscreen")
+      });
+    } else {
+      printError(response.status);
+    }
+  } catch (error) {
+    printError(error);
+  }
+}
+
+getVideo();
+
+function printError(error) {
+  console.error("Error occurred:", error);
+}
