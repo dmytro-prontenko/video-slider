e38de83a84cd80150f6fb9f7c4e65f7f7e3850ee
Dmytro Prontenko <136347904+dmytro-prontenko@users.noreply.github.com>
2024-02-13 23:21:30 +0100

    modal added

--- main.js
+++ main.js
@@ -2,23 +2,33 @@ const token = "Bearer 4e8b5ccf15e108ada37b3117bf67c112";
 import axios from "axios";
 import Swiper from "swiper";
 import "swiper/css";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 
 const videoURL = `https://api.vimeo.com/videos?query=824804225`;
 const countVideoFrames = 8;
 
-const swiper = new Swiper(".swiper", {
+const mainSwiper = new Swiper("#main-swiper", {
   slidesPerView: 4,
   initialSlide: 0,
   speed: 850,
   loop: true,
-
   modules: [Navigation],
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
+
+// const modalSwiper = new Swiper("#modal-swiper", {
+//   slidesPerView: 4,
+//   initialSlide: 0,
+//   speed: 850,
+//   loop: true,
+//   modules: [Pagination],
+//   pagination: {
+//     el: '.modal-swiper-pagination',
+//   },
+// });
 
 const options = {
   headers: {
@@ -40,7 +50,6 @@ async function getVideo() {
       const slideStartTag = "<div class='swiper-slide'>";
       const slideEndTag = "</div>";
       for (let i = 1; i <= countVideoFrames; i++) {
-        // htmlToInsert += slideStartTag + response.embed.html + slideEndTag;
         htmlToInsert +=
           slideStartTag +
           `<img
@@ -54,14 +63,29 @@ async function getVideo() {
       videoContainer.insertAdjacentHTML("afterbegin", htmlToInsert);
       const slides = document.querySelectorAll(".slide-img");
 
-      slides.forEach(slide => slide.addEventListener("click",(e)=>console.log(`${e.currentTarget}clicked, imgId - ${slide.getAttribute("id")}`)))
+      slides.forEach((slide) => {
+        slide.addEventListener("click", () => {
+          const modal = document.getElementById("myModal");
+          modal.style.display = "block";
 
-      // const iframes = document.querySelectorAll("iframe");
-      // iframes.forEach((el) => {
-      //   el.setAttribute("height", "300px"),
-      //   el.setAttribute("controls", 0),
-      //   el.setAttribute("allow", "fullscreen")
-      // });
+          window.addEventListener("keydown", (e) => {
+            if (e.key === "Escape") {
+              modal.style.display = "none";
+            }
+          });
+
+          modal.addEventListener("click", (e) => {
+            if (e.target === modal) {
+              modal.style.display = "none";
+            }
+          });
+
+          const closeBtn = document.querySelector(".close");
+          closeBtn.addEventListener("click", () => {
+            modal.style.display = "none";
+          });
+        });
+      });
     } else {
       printError(response.status);
     }
@@ -75,10 +99,3 @@ getVideo();
 function printError(error) {
   console.error("Error occurred:", error);
 }
-
-
-function toggleModal() {
-  const modal = document.getElementById("myModal");
-  const modalContent = document.getElementById("modalContent");
-}
-
